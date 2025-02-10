4899c64989065a72e85b3a5c778f7525a839a310
Ploven <plush337@gmail.com>
2025-02-10 13:39:32 +0000

    remove obsolete files

--- match-front-end/src/app/utils/types.ts
+++
@@ -1,8 +0,0 @@
-export type Song = {
-    id: number;
-    title: string;
-    artist: string;
-    runtime: string;
-    albumCover: string;
-    link: string;
-}
--- match-front-end/src/app/utils/urls.ts
+++
@@ -1,4 +0,0 @@
-const BACKEND_URL = "http://localhost:5000/api";
-export const TITLE_URL = `${BACKEND_URL}/track`;
-
-export const NEW_API_URL = "fakeurl";
