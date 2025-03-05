987d5f9aabe16f298005ef91a147b3f15d97b4fb
nuriafari <nuriafarimd@gmail.com>
2025-03-05 23:30:24 +0000

    Fixed bug

--- match-api/get_lyrics_data.sh
+++ match-api/get_lyrics_data.sh
@@ -5,6 +5,10 @@ pip install fasttext
 cd data/stored_data/
 wget http://millionsongdataset.com/sites/default/files/AdditionalFiles/mxm_dataset_train.txt.zip
 wget http://millionsongdataset.com/sites/default/files/AdditionalFiles/mxm_dataset_test.txt.zip
+unzip mxm_dataset_train.txt.zip
+unzip mxm_dataset_test.txt.zip
+rm mxm_dataset_train.txt.zip
+rm mxm_dataset_test.txt.zip
 cd ../../
 
 # 1st time it will download the fasttext model and exit. It'll take a while
